0a5aab248d6548d2eb6853cf94b8050a55b1cb33
Jordan Rhodes <jsrhodes15@gmail.com>
2023-11-24 07:42:42 -0800

    Write bytes out to VGA

--- src/main.rs
+++ src/main.rs
@@ -3,24 +3,13 @@
 
 use core::panic::PanicInfo;
 
+mod vga_buffer;
+
 static HELLO: &[u8] = b"Hello World!";
 
 #[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            let line_offset: isize = 160 * 2;
-            let char_offset_within_line: isize = i as isize * 2;
-            let color_offset_within_line: isize = i as isize * 2 + 1;
-            let char_offset = char_offset_within_line + line_offset;
-            let color_offset = color_offset_within_line + line_offset;
-
-            *vga_buffer.offset(char_offset) = byte;
-            *vga_buffer.offset(color_offset) = 0xb;
-        }
-    }
+    vga_buffer::print_something();
 
     loop {}
 }
