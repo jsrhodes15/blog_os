59e0c16379bbb2fe539a5cb552f7547452421c56
Jordan Rhodes <jsrhodes15@gmail.com>
2023-11-24 12:08:46 -0800

    Add test runner

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
 
@@ -11,6 +14,9 @@ static HELLO: &[u8] = b"Hello World!";
 pub extern "C" fn _start() -> ! {
     println!("Hello world{}", "!");
 
+    #[cfg(test)]
+    test_main();
+
     loop {}
 }
 
@@ -20,5 +26,20 @@ fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
 
     loop {}
+}
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
+
+#[test_case]
+fn trivial_assertion() {
+    print!("tivial assertion... ");
+    assert_eq!(1, 1);
+    println!("[ok]");
 }
 
