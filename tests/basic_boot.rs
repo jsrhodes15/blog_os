1883f7f8f2c8fcbf4cc82b06bdd38a679ad0ce0f
Jordan Rhodes <jsrhodes15@gmail.com>
2023-11-24 21:31:16 -0800

    Add tests  - extract test utils into lib  - add unit tests with custom_test_framework  - add integration tests with custom_test_framework

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -7,34 +7,19 @@
 use blog_os::println;
 use core::panic::PanicInfo;
 
-#[no_mangle] // don't mangle the name of this function
+#[no_mangle]
 pub extern "C" fn _start() -> ! {
-    println!("Hello world{}", "!");
-
-    #[cfg(test)]
     test_main();
 
     loop {}
 }
 
-// This function is called on panic.
-#[cfg(not(test))] // new attribute
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
-}
-
-#[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     blog_os::test_panic_handler(info)
 }
 
 #[test_case]
-fn trivial_assertion() {
-    assert_eq!(1, 1);
+fn test_println() {
+    println!("test_println output");
 }
-
-
-
